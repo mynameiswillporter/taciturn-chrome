323faae81eb9ca825fa579d96f83dd3c939179f0
mynameiswillporter <wporter@beta.(none)>
2014-10-01 23:28:45 -0400

    clean up some code an try to fix a stack trace.

--- js/background.js
+++ js/background.js
@@ -6,26 +6,31 @@ $(document).ready(function() {
 
 	chrome.runtime.onMessage.addListener(
 		function(request, sender, sendResponse) {
-			console.log(sender.tab ?
-				"from a content script:" + sender.tab.url :
-				"from the extension");
-			
-			chrome.cookies.getAll({'url': sender.tab.url}, function (cookies) {
-				console.log(cookies);
-				for (cookie in cookies)
-				{
-                    var cookieName = cookies[cookie].name;
-                    console.log(cookieName); 
-                    if (new RegExp(sessionSubstrings.join("|"),"i").test(cookieName) && !cookies[cookie].httpOnly) 
-					{
-						new Notification('Warning', {
-							icon: 'img/popup.png',
-							body: 'Insecure session cookie found! (' + cookies[cookie].domain + '/' + cookies[cookie].name + ') '
-						});
-					}
-				}
-			});
-		sendResponse({farewell: ''});
-	});
+		
+            // If we dont check this sometimes it stack traces
+            if (sender.tab) {
+	
+			    chrome.cookies.getAll({'url': sender.tab.url}, function (cookies) {
+				    console.log(cookies);
+    				for (cookie in cookies)
+	    			{
+                        var cookieName = cookies[cookie].name;
+                        console.log(cookieName); 
+                        if (new RegExp(sessionSubstrings.join("|"),"i").test(cookieName) && !cookies[cookie].httpOnly) 
+					    {
+						    new Notification('Warning', {
+							    icon: 'img/popup.png',
+							    body: 'Insecure session cookie found! (' + cookies[cookie].domain + '/' + cookies[cookie].name + ') '
+						    });
+					    }
+				    }
+			    });
+		        sendResponse({farewell: ''});
+	        } else {
+
+                // I dont fully understand sender.tab yet so putting this here just incase
+                console.log("Not processing this page because sender.tab did not exist");
+            }
+    });
 });
 
