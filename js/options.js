02d53fb33b015b3c8d9e80a4a06fef433d9e9ba5
Will Porter <will@tenex.tech>
2017-09-29 01:44:37 +0800

    Disallow adding sites more than once, and allow the removal of sites from the list.

--- js/options.js
+++ js/options.js
@@ -1,4 +1,4 @@
-function load_options() {
+function loadOptions() {
   chrome.storage.sync.get({
     ignoredDomains: [],
   },
@@ -7,38 +7,101 @@ function load_options() {
     // First removed all entries from the table, so they can
     // be repopulated.  This will eliminate any inconsistencies we may
     // have by merely updating the view.
-    $('#ignoredDomains tr').remove();
+    $('#ignoredDomains > tbody > tr > td').parent('tr').empty();
 
     // Add all of the ignored domains to the ignored domains table
     for (var index in items.ignoredDomains) {
       $('#ignoredDomains> tbody:last-child').append(
         $('<tr/>').append(
           $('<td/>').text(items.ignoredDomains[index]),
-          $('<td/>').append('<button/>')
+          $('<td/>').append(
+            $('<button/>', {
+              text: "Remove",
+              class: "btn btn-danger"
+            }).click(removeIgnoredDomain(items.ignoredDomains[index]))
+          )
         )
       );
     }
   });
 }
 
-function add_ignore_domain() {
+// This returns a function so that when we add it to the click
+// handler with predefined parameters, the inner function is not
+// executed.  Otherwise this will cause chaos.
+function removeIgnoredDomain(domain) {
+  return function(event) {
+    chrome.storage.sync.get({
+      ignoredDomains: [],
+    },
+    function(items) {
+      if (chrome.runtime.lastError) {
+        console.log(chrome.runtime.lastError.message);
+      } else {
+        var ignoredDomains = items.ignoredDomains;
+
+        // Dont add duplicate domains.
+        if (ignoredDomains.includes(domain)) {
+          var domain_index = ignoredDomains.indexOf(domain);
+          if (domain_index > -1) {
+              ignoredDomains.splice(domain_index, 1);
+          }
+          chrome.storage.sync.set({
+            ignoredDomains: ignoredDomains
+          }, function () {
+            if (chrome.runtime.lastError) {
+              console.log(chrome.runtime.lastError.message);
+            } else {
+              loadOptions();
+            }
+          });
+        }
+      }
+    });
+  }
+}
+
+function addIgnoredDomain() {
   var newDomain = $('#newDomainToIgnore').val();
   chrome.storage.sync.get({
     ignoredDomains: [],
   },
   function(items) {
-    var newDomains = items.ignoredDomains;
-    newDomains.push(newDomain);
-    chrome.storage.sync.set({
-      ignoredDomains: newDomains
-    }, function () {
-      load_options();
-      $('newDomainToIgnore').val('');
-    });
+    if (chrome.runtime.lastError) {
+      console.log(chrome.runtime.lastError.message);
+    } else {
+      var ignoredDomains = items.ignoredDomains;
+
+      // Dont add duplicate domains.
+      if (!ignoredDomains.includes(newDomain)) {
+        ignoredDomains.push(newDomain);
+        chrome.storage.sync.set({
+          ignoredDomains: ignoredDomains
+        }, function () {
+          if (chrome.runtime.lastError) {
+            console.log(chrome.runtime.lastError.message);
+          } else {
+            loadOptions();
+            $('#newDomainToIgnore').val('');
+            $('#ignoredDomainErrors').hide();
+          }
+        });
+      } else {
+        $('#ignoredDomainErrors').text('Already ignored domain: ' + newDomain);
+        $('#ignoredDomainErrors').show();
+      }
+    }
   });
 }
 
+function callback() {
+    if (chrome.runtime.lastError) {
+        console.log(chrome.runtime.lastError.message);
+    }
+}
+
 $(document).ready(function() {
-  load_options();
-  $('#addIgnoredDomain').click(add_ignore_domain);
+  loadOptions();
+  $('#ignoredDomainErrors').hide();
+  $('#addIgnoredDomain').click(addIgnoredDomain);
 });
